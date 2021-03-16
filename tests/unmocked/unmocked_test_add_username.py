8a2fd2b9541166bf6b05feecb2e43aea7a9567b8
Parth <ppatil91099@gmail.com>
2021-03-16 02:28:58 +0000

    pylinted the testcases

--- tests/unmocked/unmocked_test_add_username.py
+++ tests/unmocked/unmocked_test_add_username.py
@@ -1,76 +1,109 @@
 '''
 TO RUN FILE: python unmocked_test_add_username.py
-This file is testing how the global dictionary in python is getting updated 
+This file is testing how the username is being added to the dictionary
 '''
-import unittest, copy, os, sys, inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-two_up = os.path.dirname(os.path.dirname(current_dir))
-sys.path.insert(0, two_up)
-import app as app
+import unittest
+import copy
+import os
+import sys
+import inspect
+CURRENT_DIC = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
+TWO_UP = os.path.dirname(os.path.dirname(CURRENT_DIC))
+sys.path.insert(0, TWO_UP)
+import app as file
 
 KEY_INPUT = 'input'
 KEY_EXPECTED = 'expected'
 
 INITIAL_USERNAME = {'0': '', '1': '', 'spec': []}
 
-class test_app_functionality(unittest.TestCase):
-    
+
+class TestAppFunctionality(unittest.TestCase):
+    '''
+    This class extends unittest.TestCase and defines a couple of fucntions to
+    setup the file and to pass in a couple of testcase fucntions
+    '''
     def setUp(self):
-        app.USER_NAME =  copy.copy(INITIAL_USERNAME)
-        self.success_test_params = [
-            {
-                KEY_INPUT: 'user1',
-                KEY_EXPECTED: {'0': 'user1', '1': '', 'spec': []}
-            },
-            {
-                KEY_INPUT: 'user2',
-                KEY_EXPECTED: {'0': 'user1', '1': 'user2', 'spec': []}
+        file.USER_NAME = copy.copy(INITIAL_USERNAME)
+        self.success_test_params = [{
+            KEY_INPUT: 'user1',
+            KEY_EXPECTED: {
+                '0': 'user1',
+                '1': '',
+                'spec': []
             }
-        ]
-        self.failure_test_params = [
-            {
-                KEY_INPUT: 'user1',
-                KEY_EXPECTED: {'0': '', '1': 'user1', 'spec': []}
-            },
-            {
-                KEY_INPUT: 'user2',
-                KEY_EXPECTED: {'0': '', '1': 'user1', 'spec': ['user2']}
+        }, {
+            KEY_INPUT: 'user2',
+            KEY_EXPECTED: {
+                '0': 'user1',
+                '1': 'user2',
+                'spec': []
             }
-        ]
-        self.failure_test_params_not_in = [
-            {
-                KEY_INPUT: 'user1',
-                KEY_EXPECTED: {'0': '', '1': '', 'spec': []}
-            },
-            {
-                KEY_INPUT: 'user2',
-                KEY_EXPECTED: {'0': '', '1': 'user1', 'spec': ['user2']}
+        }]
+        self.failure_test_params = [{
+            KEY_INPUT: 'user1',
+            KEY_EXPECTED: {
+                '0': '',
+                '1': 'user1',
+                'spec': []
             }
-        ]
-        
+        }, {
+            KEY_INPUT: 'user2',
+            KEY_EXPECTED: {
+                '0': '',
+                '1': 'user1',
+                'spec': ['user2']
+            }
+        }]
+        self.failure_test_params_not_in = [{
+            KEY_INPUT: 'user1',
+            KEY_EXPECTED: {
+                '0': '',
+                '1': '',
+                'spec': []
+            }
+        }, {
+            KEY_INPUT: 'user2',
+            KEY_EXPECTED: {
+                '0': '',
+                '1': 'user1',
+                'spec': ['user2']
+            }
+        }]
+
     def test_succesful_params(self):
+        '''
+        Successfull params are being looped over to make sure they are returning expected values
+        '''
         for test in self.success_test_params:
-            actual_result = app.add_user(test[KEY_INPUT]) 
+            actual_result = file.add_user(test[KEY_INPUT])
             expected_result = test[KEY_EXPECTED]
             self.assertEqual(actual_result, expected_result)
             self.assertEqual(actual_result['0'], expected_result['0'])
             self.assertEqual(actual_result['1'], expected_result['1'])
 
     def test_failure_params(self):
+        '''
+        Failure params are being looped over to make sure they are not returning expected values
+        '''
         for test in self.failure_test_params:
-            actual_result = app.add_user(test[KEY_INPUT])
+            actual_result = file.add_user(test[KEY_INPUT])
             expected_result = test[KEY_EXPECTED]
             self.assertNotEqual(actual_result, expected_result)
             self.assertNotEqual(actual_result['0'], expected_result['0'])
             self.assertNotEqual(actual_result['1'], expected_result['1'])
-    
+
     def test_failure_params_not_int(self):
+        '''
+        Failure params are being looped over to make sure they are not returning expected values
+        '''
         for test in self.success_test_params:
-            actual_result = app.add_user(test[KEY_INPUT]) 
+            actual_result = file.add_user(test[KEY_INPUT])
             expected_result = test[KEY_EXPECTED]
             self.assertDictEqual(actual_result, expected_result)
             self.assertDictContainsSubset(actual_result, expected_result)
 
-        
+
 if __name__ == '__main__':
     unittest.main()
--- tests/unmocked/unmocked_test_update_board.py
+++ tests/unmocked/unmocked_test_update_board.py
@@ -6,43 +6,44 @@ import unittest
 import unittest.mock as mock
 from unittest.mock import patch
 import unittest, copy, os, sys, inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+current_dir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
 two_up = os.path.dirname(os.path.dirname(current_dir))
 sys.path.insert(0, two_up)
 import app as app
-import models 
+import models
 
 KEY_INPUT = "input"
 KEY_EXPECTED = "expected"
-INITIAL_BOARD = ['','','','','','','','','']
+INITIAL_BOARD = ['', '', '', '', '', '', '', '', '']
 # INITIAL_USERNAM
+
 
 class DBTestCase(unittest.TestCase):
     def setUp(self):
         app.UPDATE_BOARD = copy.copy(INITIAL_BOARD)
         self.success_test_params = [
             {
-                KEY_INPUT: ['','X','','','X','','','','X'],
+                KEY_INPUT: ['', 'X', '', '', 'X', '', '', '', 'X'],
                 KEY_EXPECTED: (3, 0)
             },
             {
-                KEY_INPUT: ['','X','','O','','O','','O',''],
+                KEY_INPUT: ['', 'X', '', 'O', '', 'O', '', 'O', ''],
                 KEY_EXPECTED: (1, 3)
             },
         ]
-        
+
         self.failure_test_params = [
             {
-                KEY_INPUT: ['','X','','','X','','','','X'],
+                KEY_INPUT: ['', 'X', '', '', 'X', '', '', '', 'X'],
                 KEY_EXPECTED: (2, 1)
             },
             {
-                KEY_INPUT: ['','X','','O','','O','','O',''],
+                KEY_INPUT: ['', 'X', '', 'O', '', 'O', '', 'O', ''],
                 KEY_EXPECTED: (3, 1)
             },
         ]
-        
-    
+
     def test_succesful_params(self):
         for test in self.success_test_params:
             actual_result = app.update_mock_board_index(test[KEY_INPUT])
@@ -50,7 +51,7 @@ class DBTestCase(unittest.TestCase):
             self.assertEqual(actual_result, expected_result)
             self.assertEqual(actual_result[0], expected_result[0])
             self.assertEqual(actual_result[1], expected_result[1])
-    
+
     def test_failure_params(self):
         for test in self.failure_test_params:
             actual_result = app.update_mock_board_index(test[KEY_INPUT])
@@ -58,7 +59,7 @@ class DBTestCase(unittest.TestCase):
             self.assertNotEqual(actual_result, expected_result)
             self.assertNotEqual(actual_result[0], expected_result[0])
             self.assertNotEqual(actual_result[1], expected_result[1])
-    
+
     def test_successful_tuple(self):
         for test in self.success_test_params:
             actual_result = app.update_mock_board_index(test[KEY_INPUT])
@@ -66,6 +67,7 @@ class DBTestCase(unittest.TestCase):
             self.assertTupleEqual(actual_result, expected_result)
             self.assertIsNotNone(actual_result)
             self.assertIsNotNone(expected_result)
+
 
 if __name__ == '__main__':
     unittest.main()
